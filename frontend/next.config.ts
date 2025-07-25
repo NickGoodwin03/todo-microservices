26647a06fd1e22843f27474cf0378f6cb7a60b4a
NickGoodwin03 <Nick@goodwin-house.com>
2025-07-25 18:14:46 -0400

    Updated package.json and next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',  // enables static export
-  trailingSlash: true, // GitHub Pages needs trailing slashes
 };
 
 export default nextConfig;
